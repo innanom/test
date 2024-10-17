9b41555ff24886a28569af496332ff74bb70c2ef
Inna Nomerovkina <inkosof@gmail.com>
2024-10-17 12:58:21 +0300

    finish styles section clients

--- js/we__gallery.js
+++ js/we__gallery.js
@@ -7,6 +7,7 @@ const swiper = new Swiper('.swiper', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+    lockClass: 'swiper-button'
   },
 
     slidesPerView: 'auto',
